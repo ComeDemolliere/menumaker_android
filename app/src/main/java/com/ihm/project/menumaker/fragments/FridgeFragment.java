2ca6bfee4998bad6e5a74830c8c8082af704b894
Florian AINADOU <florian.ainadou97@gmail.com>
2019-04-29 18:03:11 +0200

    Fridge initiation

--- app/src/main/java/com/ihm/project/menumaker/fragments/FridgeFragment.java
+++ app/src/main/java/com/ihm/project/menumaker/fragments/FridgeFragment.java
@@ -3,10 +3,12 @@ package com.ihm.project.menumaker.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.ihm.project.menumaker.R;
 
@@ -16,9 +18,18 @@ public class FridgeFragment extends Fragment {
         return new FridgeFragment();
     }
 
+    private TabLayout provisions;
+    private TabLayout shoppinglist;
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        final View fridgeView = inflater.inflate(R.layout.home_fragment, container, false);
+
+        ListView myIngredients = fridgeView.findViewById(R.id.provisions);
+        ListView myShoppingList = fridgeView.findViewById(R.id.shopping_list);
         return inflater.inflate(R.layout.fridge_fragment, container, false);
     }
 
