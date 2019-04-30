8c6864d5ee7b215e8c377776a07a4667e58e14d0
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-04-30 17:15:55 +0200

    add dish validate fragment

--- app/src/main/java/com/ihm/project/menumaker/fragments/DishFinderFragment.java
+++ app/src/main/java/com/ihm/project/menumaker/fragments/DishFinderFragment.java
@@ -1,27 +1,17 @@
 package com.ihm.project.menumaker.fragments;
 
-import android.app.Activity;
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-
 import com.ihm.project.menumaker.R;
-import com.ihm.project.menumaker.Samples.Dish;
 import com.ihm.project.menumaker.adapters.DishFinderSlidePagerAdapter;
-import com.ihm.project.menumaker.adapters.DishesAdapter;
-import com.ihm.project.menumaker.models.Dishes;
 
 public class DishFinderFragment extends Fragment {
 
---
+++ app/src/main/java/com/ihm/project/menumaker/fragments/ValidateDishFragment.java
@@ -0,0 +1,40 @@
+package com.ihm.project.menumaker.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+import com.ihm.project.menumaker.R;
+
+public class ValidateDishFragment extends Fragment {
+
+    private EditText dateText;
+    private CalendarView calendar;
+
+    public static ValidateDishFragment newInstance() {
+        return new ValidateDishFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.validate_dish_fragment, container, false);
+
+        calendar = view.findViewById(R.id.calendarView);
+        dateText = view.findViewById(R.id.date);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                dateText.setText("" + dayOfMonth);
+            }
+        });
+        return view;
+    }
+}
