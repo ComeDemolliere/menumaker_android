e022f20e7679946a498ce8085e2e79c0d5ec80c4
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-05-06 09:05:27 +0200

    add tabLayout for fridge fragment

--- app/src/main/java/com/ihm/project/menumaker/adapters/FridgeSlidePagerAdapter.java
+++ app/src/main/java/com/ihm/project/menumaker/adapters/FridgeSlidePagerAdapter.java
@@ -4,25 +4,34 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import com.ihm.project.menumaker.fragments.FridgeProvisions;
-import com.ihm.project.menumaker.fragments.FridgeToBuyList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FridgeSlidePagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
 
     public FridgeSlidePagerAdapter(FragmentManager fm) {
         super(fm);
     }
+
     @Override
     public Fragment getItem(int i) {
-            if (i==0) return new FridgeProvisions();
-
-            else if (i==1)return new FridgeToBuyList();
-
-            return null;
+        return mFragmentList.get(i);
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
     }
 }
